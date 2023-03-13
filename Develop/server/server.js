8b7be5ab4cf0e7f6614937afa74dd5f6e1cb6b9d
graysonwwagner <graysonwwagner@gmail.com>
2023-03-13 11:15:17 -0500

    server.js file api configs add

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
 
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
-
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+app.listen(PORT, function () {
+  console.log(` Bruh ==> API Server now listening on PORT ${PORT}!`);
 });
